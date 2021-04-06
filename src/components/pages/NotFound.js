6892eeb408d453394f98b691a8f8232fec5e7a69
moishe <moishe822@gmail.com>
2021-04-06 17:45:37 -0400

    Final commit i think

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <a href="http://localhost:3000/" className="lead">The page you are looking for does not exist</a>
+
+        </div>
+    )
+}
+
+export default NotFound
